2d934fc4e794271280aa282b7b813dd4e5285840
junya <junya@JUNYA>
2023-12-05 02:26:47 +0300

    Добавьте файлы проекта.

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary> 
+    public partial class MainWindow : Window
+    {
+        private double currentValue = 0;
+        private string currentOperator = "";
+        private bool isNewNumber = true;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Digit_Click(object sender, RoutedEventArgs e)
+        {
+            if (isNewNumber)
+            {
+                Display.Text = ((Button)sender).Content.ToString();
+                isNewNumber = false;
+            }
+            else
+            {
+                if (Display.Text == "0")
+                    Display.Text = ((Button)sender).Content.ToString();
+                else
+                    Display.Text += ((Button)sender).Content.ToString();
+            }
+        }
+        private void Operator_Click(object sender, RoutedEventArgs e)
+        {
+            currentOperator = ((Button)sender).Content.ToString();
+            currentValue = double.Parse(Display.Text);
+            isNewNumber = true;
+        }
+
+        private void Equals_Click(object sender, RoutedEventArgs e)
+        {
+            double newValue = double.Parse(Display.Text);
+            switch (currentOperator)
+            {
+                case "+":
+                    currentValue += newValue;
+                    break;
+                case "-":
+                    currentValue -= newValue;
+                    break;
+                case "X":
+                    currentValue *= newValue;
+                    break;
+                case "/":
+                    if (newValue != 0)
+                        currentValue /= newValue;
+                    else
+                        MessageBox.Show("Cannot divide by zero!");
+                    break;
+            }
+            Display.Text = currentValue.ToString();
+            isNewNumber = true;
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            currentValue = 0;
+            currentOperator = "";
+            Display.Text = "0";
+        }
+
+        private void Negate_Click(object sender, RoutedEventArgs e)
+        {
+            if (Display.Text != "0")
+            {
+                double value = double.Parse(Display.Text);
+                value = -value;
+                Display.Text = value.ToString();
+            }
+        }
+
+        private void Percentage_Click(object sender, RoutedEventArgs e)
+        {
+            if (Display.Text != "0")
+            {
+                double value = double.Parse(Display.Text);
+                value /= 100;
+                Display.Text = value.ToString();
+            }
+        }
+
+        private void Decimal_Click(object sender, RoutedEventArgs e)
+        {
+            if (!Display.Text.Contains(","))
+                Display.Text += ",";
+        }
+
+        private void Sqrt_Click(object sender, RoutedEventArgs e)
+        {
+            Button clickedButtion = (Button)sender;
+            double value = double.Parse(Display.Text);
+            double result = 0;
+             switch(clickedButtion.Content.ToString())
+             {
+                 case "x²":
+                     result = Math.Pow(value, 2);
+                     Display.Text = result.ToString();
+                     break;
+                 case "√":
+                     if (value >= 0)
+                     {
+                         result = Math.Sqrt(value);
+                         Display.Text = result.ToString();
+                     }
+                     else
+                     {
+                         Display.Text = "Error: Invalid input";
+                     }
+                     break;
+             }      
+        }
+    } 
+}
+
